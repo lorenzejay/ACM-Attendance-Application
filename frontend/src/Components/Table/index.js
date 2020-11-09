bbbd7824fd6d0edca0e2fa6839654556e8bac6d1
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-08 18:20:07 -0800

    removed a div from the table

--- frontend/src/Components/Table/index.js
+++ frontend/src/Components/Table/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./styles.scss";
 
 const TableContainer = ({ children }) => {
-  return (
-    <div style={{ overflowX: "auto" }}>
-      <table>{children}</table>;
-    </div>
-  );
+  return <table>{children}</table>;
 };
 
 export default TableContainer;
